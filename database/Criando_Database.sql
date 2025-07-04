7daadf106d46cf363412ce53e76bc695e05e3af7
Lucas José <lucasjds012@gmail.com>
2025-07-03 21:07:58 -0300

    Um nova database foi feita para o projeto, alinhando melhor com as funcionalidades iniciais que ele terá

--- database/Criando_Database.sql
+++ database/Criando_Database.sql
@@ -1,31 +1,52 @@
-CREATE DATABASE biblioteca;
-Use biblioteca;
+create database biblioteca
+character set utf8mb4
+collate utf8mb4_unicode_ci;
 
-CREATE TABLE Cartao(
-Id int PRIMARY KEY AUTO_INCREMENT,
-Validade date NOT NULL
+use biblioteca;
+
+CREATE TABLE Usuario(
+Id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Nome varchar(50) NOT NULL,
+Email varchar(100) NOT NULL UNIQUE,
+Senha varchar(100) NOT NULL,
+Nacionalidade varchar(56),
+DataCadastro date NOT NULL
 );
 
-CREATE TABLE Leitor(
-Id int PRIMARY KEY AUTO_INCREMENT,
-Nome varchar(50) NOT NULL,
-Email varchar(50) NOT NULL,
-Cartao_Id int,
-CONSTRAINT ´FK_LEITOR_CARTAO´ FOREIGN KEY (Cartao_Id) REFERENCES Cartao(Id)
+CREATE TABLE Categoria(
+Id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+Genero char(30) NOT NULL,
+SubGenero char(30)
+);
+
+CREATE TABLE Avaliacao(
+Id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+Usuario_Id int UNSIGNED,
+Livro_Id int UNSIGNED,
+Texto text NOT NULL
 );
 
 CREATE TABLE Livro(
-Id int PRIMARY KEY AUTO_INCREMENT,
+Id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 Titulo varchar(50) NOT NULL,
-Autor varchar(50) NOT NULL,
-Ano_Publicacao int NOT NULL
+AnoPublicacao year NOT NULL,
+Genero_Id int UNSIGNED,
+Sinopse text,
+Avaliacao_Id int UNSIGNED,
+ISBN char(20) UNIQUE,
+
+CONSTRAINT FK_Livro_Categoria
+FOREIGN KEY (Genero_Id) REFERENCES Categoria(Id),
+
+CONSTRAINT FK_Livro_Avaliacao
+FOREIGN KEY (Avaliacao_Id) REFERENCES Avaliacao(Id)
 );
 
-CREATE TABLE Leitor_Livro(
-Leitor_Id int,
-Livro_Id int,
-Data_Emprestimo date NOT NULL,
-Data_Devolucao date,
-CONSTRAINT ´FK_LEITOR_LIVRO_LEITOR´ FOREIGN KEY (Leitor_Id) REFERENCES Leitor(Id),
-CONSTRAINT ´FK_LEITOR_LIVRO_LIVRO´ FOREIGN KEY (Livro_Id) REFERENCES Livro(Id)
+CREATE TABLE AutorLivro(
+Livro_Id int UNSIGNED NOT NULL,
+Autor_Id int UNSIGNED NOT NULL,
+PRIMARY KEY (Livro_Id, Autor_Id),
+
+FOREIGN KEY (Livro_Id) REFERENCES Livro(Id),
+FOREIGN KEY (Autor_Id) REFERENCES Usuario(Id)
 );
